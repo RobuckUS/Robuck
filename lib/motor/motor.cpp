8f23ee5853e18dd3fc6f36582f414f081016c8aa
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2019-09-24 12:05:12 -0400

    Make the robot advance 3 inch and stop

--- lib/motor/motor.cpp
+++ lib/motor/motor.cpp
@@ -4,14 +4,39 @@
 #define DIR_LEFT
 
 /** Function to control the two DC motors on robots
-
-@param speed, reprensents direction and amplitude of PWM
-floating value between [-1.0, 1.0]
+ * 
+ * @param distance, reprensents direction and distance of the robot.
+ * floating value in centimer between [-1.0, 1.0]
 */
-void motor_walk(float speed)
+void motor_walk(float distance)
 {
-    MOTOR_SetSpeed(LEFT,speed);
-    MOTOR_SetSpeed(RIGHT,speed);
+    if (ENCODER_Read(LEFT) < 3200 )
+    {
+        MOTOR_SetSpeed(LEFT,0.1);
+    }
+    else
+    {
+        MOTOR_SetSpeed(LEFT,0);
+    }
+    
+    if (ENCODER_Read(RIGHT) < 3200 )
+    {
+        MOTOR_SetSpeed(RIGHT,0.1);
+    }
+    else
+    {
+        MOTOR_SetSpeed(RIGHT,0);
+    }
+    
+    
+    Serial.println(ENCODER_Read(LEFT));
+    Serial.println(ENCODER_Read(RIGHT));
+    Serial.println("\n");
+
+
+
+    //MOTOR_SetSpeed(LEFT,speed);
+    //MOTOR_SetSpeed(RIGHT,speed);
 }
 
 void motor_turn(float direction, float speed)
