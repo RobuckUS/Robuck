c74b47a007132f857ff054cf4d275bd3d9b1d974
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2019-10-25 17:57:20 -0400

    Remove useless comment and auto-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-/*
-Projet: Le nom du script
-Equipe: Votre numero d'equipe
-Auteurs: Les membres auteurs du script
-Description: Breve description du script
-Date: Derniere date de modification
-*/
-
 /* ****************************************************************************
 Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
@@ -13,42 +5,28 @@ Inclure les librairies de functions que vous voulez utiliser
 #include <Arduino.h>
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
 
-
-
 /* ****************************************************************************
 Variables globales et defines
 **************************************************************************** */
-// -> defines...
-// L'ensemble des fonctions y ont acces
-
-
 
 /* ****************************************************************************
-Vos propres fonctions sont creees ici
+Fonctions
 **************************************************************************** */
-void maFonction(){
-  // code
-}
-
 
 /* ****************************************************************************
-Fonctions d'initialisation (setup)
+Fonctions d'initialisation
 **************************************************************************** */
-// -> Se fait appeler au debut du programme
-// -> Se fait appeler seulement un fois
-// -> Generalement on y initilise les varibbles globales
 
-void setup(){
+void setup()
+{
   BoardInit();
 }
 
-
 /* ****************************************************************************
-Fonctions de boucle infini (loop())
+Fonctions de boucle infini
 **************************************************************************** */
-// -> Se fait appeler perpetuellement suite au "setup"
 
-void loop() {
+void loop()
+{
   // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
-  delay(10);// Delais pour décharger le CPU
 }
