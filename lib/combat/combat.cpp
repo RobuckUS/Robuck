f7eb39c1a2632382054376396df55682f7958e63
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2019-11-02 05:25:37 -0400

    Add main robot functions in combat

--- lib/combat/combat.cpp
+++ lib/combat/combat.cpp
@@ -1,2 +1,15 @@
 #include "combat.h"
+#include <sens.h>
 #include <LibRobus.h>
+
+void combat_robot1()
+{
+    sens_followLineIR();
+    //Code for robot 1 here
+}
+
+void combat_robot2()
+{
+    sens_followLineIR();
+    //code for robot 2 here
+}
