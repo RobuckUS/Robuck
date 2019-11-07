c0418e55de463193b78a4b5c522047ed3fbafe24
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2019-11-07 04:45:01 -0500

    Update main for the real WAR!

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,18 @@ Variables globales et defines
 
 #define JEAN 31
 #define GUY 43
-#define debugg 100
 
-const int g_robot_name = GUY;
+#define DELAY false
+
+//Choose JEAN or GUY
+const int g_robot_name = JEAN;
+
+//Choose RED, GREEN, BLUE or YELLOW
+const combat_color_t targetColor1 = RED;
+const combat_color_t targetColor2 = RED;
 
 /* ****************************************************************************
-Fonctions 
+Fonctions
 **************************************************************************** */
 
 /* ****************************************************************************
@@ -29,7 +35,6 @@ Fonctions d'initialisation
 
 void setup()
 {
-
     BoardInit();
     sens_init();
     Serial.begin(9600);
@@ -42,54 +47,28 @@ Fonctions de boucle infini
 
 void loop()
 {
-    //Go to War()
+    // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
+
+    while (!ROBUS_IsBumper(REAR))
+        ; //Wait for rear bumper press
+
     switch (g_robot_name)
     {
     case JEAN:
-        Serial.print("Jean POWER!!");
-        if (ROBUS_IsBumper(LEFT))
-        {
-            combat_robot1(YELLOW);
-        }
-        if (ROBUS_IsBumper(RIGHT))
-        {
-            combat_robot1(BLUE);
-        }
-        if (ROBUS_IsBumper(FRONT))
-        {
-            combat_robot1(GREEN);
-        }
-        if (ROBUS_IsBumper(REAR))
-        {
-            combat_robot1(RED);
-        }
+        combat_robot1(targetColor1);
         break;
 
     case GUY:
-        if (ROBUS_IsBumper(LEFT))
-        {
-            combat_robot2(YELLOW);
-        }
-
-        if (ROBUS_IsBumper(RIGHT))
-        {
-            combat_robot2(BLUE);
-        }
-
-        if (ROBUS_IsBumper(FRONT))
-        {
-            combat_robot2(GREEN);
-        }
-
-        if (ROBUS_IsBumper(REAR))
-        {
-            combat_robot2(RED);
-        }
+#if DELAY
+        delay(60000);
+#endif
+        combat_robot2(targetColor2);
         break;
 
     default:
         Serial.println("The should be a name to the robot. Please set g_robot_name");
-
+        while (1)
+            ; // halt!
         break;
     }
 }
